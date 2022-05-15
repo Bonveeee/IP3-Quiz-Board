192f42e8919cf9825f63ce0103ac38b939d5c090
bonveeee <bonochieng@gmail.com>
2022-05-15 20:14:20 +0300

    update changes on files

--- assets/Js/quizboard.js
+++ assets/Js/quizboard.js
@@ -17,7 +17,7 @@ function result() {
         score++;
     }
 
-// output of the result
+    // output of the result
     document.write("<h1>Your Result is:" + score * 10 + "%<h1>" +
         "<p>Excellent>=80%, Very Good>=70%,Average>=50%, Fail is below 50% <br> Refresh to start again!<p>"
     );
