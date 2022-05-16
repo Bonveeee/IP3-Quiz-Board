571abf705cea4e8a40ea92356483fda5c399e228
bonveeee <bonochieng@gmail.com>
2022-05-16 20:24:59 +0300

    assets/Js/results.js
    made results to be the primary js file

--- assets/Js/results.js
+++ assets/Js/results.js
@@ -1,4 +1,24 @@
+function result() {
+    var score = 0;
+    if (document.getElementById('correct 1').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 2').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 3').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 4').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 5').checked) {
+        score++;
+    }
 
+
+    window.location.href = `./results.html?result=${(score/5)*100}%`
+}
 
 const searchUrl = window.location.search;
 
